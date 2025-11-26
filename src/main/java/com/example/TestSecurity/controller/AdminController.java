b47ed5bbdaf9c206a344497ffa8e3f847a432e15
Heidar <srlimvp@gmail.com>
2025-11-26 15:25:58 +0900

    인가 config setting + login form

---
+++ src/main/java/com/example/TestSecurity/controller/AdminController.java
@@ -0,0 +1,11 @@
+package com.example.TestSecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class AdminController {
+    @GetMapping("/admin")
+    public String adminPage() {
+
+        return "admin";
+    }
+}
