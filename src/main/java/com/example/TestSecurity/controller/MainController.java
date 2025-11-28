962bcc189542289514e95c144233b9a5343510b9
Heidar <srlimvp@gmail.com>
2025-11-28 16:53:24 +0900

    session information

--- src/main/java/com/example/TestSecurity/controller/MainController.java
+++ src/main/java/com/example/TestSecurity/controller/MainController.java
@@ -1,12 +1,34 @@
 package com.example.TestSecurity.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+
+        // login user id value
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        // role value
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("id",id);
+        model.addAttribute("role",role);
+
 
         return "main";
     }
